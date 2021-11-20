8d75d39b6bf4699190822a38be54696a886a18b5
Your Name <debra4117@gmail.com>
2021-11-20 16:07:37 +0800

    update

---
+++ src/LRUCache146.java
@@ -0,0 +1,32 @@
+/**
+ * 146. LRU Cache
+ * 
+ * Q:
+ *  - Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+ *  
+ *  - implement the LRUCache class:
+ *  
+ *     - LRUCache(int capacity)
+ *       - Initialize the LRU cache with positive size capacity
+ *       
+ *     - int get(int key)
+ *       - Return the value of the key if the key exists, otherwise return -1.
+ *       
+ *     - void put(int key, int value)
+ *       - Update the value of the key if the key exists.
+ *       - Otherwise, add the key-value pair to the cache.
+ *       - If the number of keys exceeds the capacity from this operation, evict the least recently used key. 
+ *       
+ *   - The function get and put must each run in o(1) average time complexity.
+ *   
+ *   
+ * ex 1:
+ * 
+ *   - Input :
+ *   
+ * 
+ *
+ */
+public class LRUCache146 {
+
+}
