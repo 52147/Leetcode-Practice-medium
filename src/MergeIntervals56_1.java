3a1d6523e0a7e0d1db5668bd8d7ffbca6437f131
Your Name <debra4117@gmail.com>
2022-01-21 17:04:25 +0800

    update

--- src/MergeIntervals56_1.java
+++ src/MergeIntervals56_1.java
@@ -168,3 +168,34 @@ class MergeIntervals56_1 {
         return merged.toArray(new int[merged.size()][]);
     }
 }
+
+/**
+ * Complexity Analysis:
+ * 
+ *  - Time complexity:
+ *    - O(n^2)
+ *    - building the graph costs O(V+E) = O(V) + O(E) = O(n) + O(n^2) = O(n^2) time, 
+ *      as in the worst case all intervals are mutually overlapping.
+ *    - Traversing the graph has the same cost (although it might appear higher at first) 
+ *      because our visited set guarantees that each node will be visited exactly once.
+ *    - Finally, because each node is part of exactly one component,
+ *      the merge step cost O(V) = O(n) time.
+ *      
+ *    - This all adds up as follows:
+ *      O(n^2) + O(n^2) + O(N) = O(n^2)
+ *      
+ *  - Space complexity: O(n^2)
+ *  
+ *    - As previous mentioned, in the worst case, all intervals are mutually overlapping,
+ *      so there will be an edge for every pair of intervals.
+ *    - Therefore, the memory footprint is quadratic in the input size.
+ *     
+ * 
+ */
+
+
+
+
+
+
+
